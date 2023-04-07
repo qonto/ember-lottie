b0e375ecbce758fa3405f2b7fefd610e99b47f9a
Anastasia Oudin <anastasiia.oudin@qonto.com>
2023-04-07 10:29:49 +0200

    chore: use maybeEmbroider

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -3,8 +3,12 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {});
+  let app = new EmberApp(defaults, {
+    autoImport: {
+      watchDependencies: ['ember-lottie'],
+    },
+  });
 
-  const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack);
+  const { maybeEmbroider } = require('@embroider/test-setup');
+  return maybeEmbroider(app, {});
 };
