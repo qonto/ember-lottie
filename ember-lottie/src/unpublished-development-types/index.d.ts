b77bb54091f7c1c8f960350512007b2daa0c135d
Vincent Scavinner <vincent.scavinner@qonto.com>
2023-10-13 16:39:49 +0200

    feat: use official ember-source TypeScript types public preview in addon package

--- ember-lottie/src/unpublished-development-types/index.d.ts
+++ ember-lottie/src/unpublished-development-types/index.d.ts
@@ -2,7 +2,11 @@
 // These will *not* be published as part of your addon, so be careful that your published code does not rely on them!
 
 import '@glint/environment-ember-loose';
+
 import Modifier from 'ember-modifier';
+
+import 'ember-source/types';
+import 'ember-source/types/preview';
 
 declare class RenderModifier<
   Args extends Array<unknown> = Array<unknown>
